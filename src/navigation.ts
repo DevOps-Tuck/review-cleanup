3542ee3e4007581cd6705d867fd822e583ca959d
Tucker Norman <tucker@fullmeasure.io>
2024-10-09 14:41:03 -0400

    Updating ransom buttons on site- Adding BE to the /contact form.

--- src/navigation.ts
+++ src/navigation.ts
@@ -13,38 +13,22 @@ export const headerData = {
           text: 'Startup',
           href: getPermalink('/homes/startup'),
         },
-        {
-          text: 'Personal',
-          href: getPermalink('/homes/personal'),
-        },
       ],
     },
     {
       text: 'SERVICES',
       links: [
         {
-          text: 'Features (Anchor Link)',
-          href: getPermalink('/#features'),
-        },
-        {
           text: 'Services',
-          href: getPermalink('/services'),
+          href: getPermalink('/#features'),
         },
         {
           text: 'Pricing',
           href: getPermalink('/pricing'),
         },
         {
-          text: 'About us',
-          href: getPermalink('/about'),
-        },
-        {
           text: 'Contact',
           href: getPermalink('/contact'),
-        },
-        {
-          text: 'Terms',
-          href: getPermalink('/terms'),
         },
         {
           text: 'Privacy policy',
@@ -56,10 +40,6 @@ export const headerData = {
       text: 'ABOUT',
       links: [
         {
-          text: 'Click-Through',
-          href: getPermalink('/landing/click-through'),
-        },
-        {
           text: 'Product Details (or Services)',
           href: getPermalink('/landing/product'),
         },
@@ -70,7 +50,7 @@ export const headerData = {
       ],
     }
   ],
-  actions: [{ text: 'Click Here ;)', href: 'https://halyardandhelm.com', target: '_blank' }],
+  actions: [{ text: 'CLICK HERE to get started', href: getPermalink('/pricing'), target: '_blank' }],
 };
 
 export const footerData = {
@@ -116,7 +96,7 @@ export const footerData = {
         { text: 'Press', href: '#' },
         { text: 'Inclusion', href: '#' },
         { text: 'Social Impact', href: '#' },
-        { text: 'Shop', href: '#' },
+        { text: 'Shop', href: 'https://www.instagram.com/halyardandhelm/' },
       ],
     },
   ],
@@ -126,10 +106,9 @@ export const footerData = {
   ],
   socialLinks: [
     { ariaLabel: 'X', icon: 'tabler:brand-x', href: '#' },
-    { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: '#' },
+    { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: 'https://www.instagram.com/halyardandhelm/' },
     { ariaLabel: 'Facebook', icon: 'tabler:brand-facebook', href: '#' },
-    { ariaLabel: 'RSS', icon: 'tabler:rss', href: getAsset('/rss.xml') },
-    { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/onwidget/astrowind' },
+    { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/DevOps-Tuck' },
   ],
   footNote: `
     <img class="w-5 h-5 md:w-6 md:h-6 md:-mt-0.5 bg-cover mr-1.5 rtl:mr-0 rtl:ml-1.5 float-left rtl:float-right rounded-sm" src="https://onwidget.com/favicon/favicon-32x32.png" alt="onWidget logo" loading="lazy"></img>
